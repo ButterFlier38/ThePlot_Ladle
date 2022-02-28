69dd7b5c0ba736812866a4f6da48eca5280ce201
JasmineAufiero <jasmineaufiero@gmail.com>
2022-02-28 19:25:45 +0100

    adding the shakingView

---
+++ Ladle_thePlot/Views/ShakingView.swift
@@ -0,0 +1,50 @@
+//
+//  ShakingView.swift
+//  Ladle_thePlot
+//
+//  Created by Jasmine Aufiero on 28/02/22.
+//
+
+import SwiftUI
+
+struct ShakingView: View {
+    @Binding var currentScene :Int
+    @State var enabled :Bool = false
+    
+    var body: some View {
+        
+        GeometryReader { geometry in
+            VStack {
+                
+                Image("nuvoletta")
+                    .resizable()
+                    .scaledToFit()
+                    .overlay(Image(systemName: "ipad.homebutton")
+                                .resizable()
+                                .scaledToFit()
+                                .padding().padding(10)
+                                .multilineTextAlignment(.center)
+                                .rotationEffect(.degrees(90)))
+                    .frame(height: geometry.size.height * 0.2, alignment: .bottom)
+                    
+                // adding shaking animation on image
+                
+                
+            }.onShake {
+                // adding the final result of the recipe
+                
+                currentScene += 1
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
+//struct ShakingView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ShakingView()
+//    }
+//}
