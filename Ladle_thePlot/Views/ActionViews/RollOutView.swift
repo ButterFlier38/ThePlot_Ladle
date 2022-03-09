b3608935f9a491e11882853ae19aad560a83c53d
JasmineAufiero <jasmineaufiero@gmail.com>
2022-03-09 16:41:23 +0100

    roll out view fixed

--- Ladle_thePlot/Views/ActionViews/RollOutView.swift
+++ Ladle_thePlot/Views/ActionViews/RollOutView.swift
@@ -20,68 +20,58 @@ struct RollOutView : View{
     
     @State var isClicked : Bool = false
     @State var count : Int = 0
-   
+    
     var body: some View{
         GeometryReader { geometry in
             ZStack {
+   
+                Image(change ? ((image2 != "none") ? image2 : image1) : image1)
+                    .resizable()
+                    .scaleEffect(0.6)
+                    .aspectRatio(1.2, contentMode: .fit)
+                    .position(x: geometry.size.width - geometry.size.width/2, y: geometry.size.height - geometry.size.height/2)
                 
-                if !change {
-                    
-                    Image(image1)
-                        .resizable()
-                        .scaleEffect(0.6)
-                        .aspectRatio(1.2, contentMode: .fit)
-                        .position(x: geometry.size.width - geometry.size.width/2, y: geometry.size.height - geometry.size.height/2)
-                      
-                    Image("mattarello")
-                        .resizable()
-                        .scaleEffect(0.7)
-                        .scaledToFit()
-                        .rotationEffect(.degrees(90))
-                        .offset(y: isClicked ? -85 :85)
-                        .position(x: geometry.size.width - geometry.size.width/2, y: geometry.size.height - geometry.size.height/1.7)
-                        .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: moveMattarello)
-                        .onTapGesture{
-                            count += 1
-                            isClicked.toggle()
-                            moveMattarello =  0.90
-                            
-                            if count > 2 {
-                                change.toggle()
-                            }
-                            
-                                           
-                        }
-
-                }
-                
-                
-                else {
-                    
-                        Image((image2 != "none") ? image2 : image1)
-                            .resizable()
-        //                    .scaleEffect(scale)
-                            .aspectRatio(1.2, contentMode: .fit)
-                            .position(x: geometry.size.width - geometry.size.width/2, y: geometry.size.height - geometry.size.height/2)
-
-                    
-                    Button{
-                        currentScene += 1
-                    }label: {
+                Image("mattarello")
+                    .resizable()
+                    .scaleEffect(0.7)
+                    .scaledToFit()
+                    .rotationEffect(.degrees(90))
+                    .offset(y: isClicked ? -85 :85)
+                    .position(x: geometry.size.width - geometry.size.width/2, y: geometry.size.height - geometry.size.height/1.7)
+                    .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: moveMattarello)
+                    .onTapGesture{
+                        count += 1
+                        isClicked.toggle()
+                        moveMattarello =  0.90
                         
-                        NextStepButton()
-                            
-                    } .frame(width: geometry.size.width * 0.2,height: geometry.size.height * 0.2, alignment: .bottomTrailing)
-               .position(x: geometry.size.width * move, y: geometry.size.height * 0.99)
-    //           .disabled(scale != 0.7)
-                        .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: move)
-                        .onAppear{
-                            move =  0.99
+                        if count == 2 {
+                            change.toggle()
                         }
-                } // :else
-            }
-
- 
+                        
+                    } // :on tap gesture
+                
+                Image("nuvoletta")
+                    .resizable()
+                //                            .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
+                    .overlay(
+                        Text(LocalizedStringKey(String("Tap to roll out the dough!")))
+                            .scaledToFit()
+                            .font(Font.custom("HappyMonkey-Regular", size:  geometry.size.height > geometry.size.width ? geometry.size.width * 0.2: geometry.size.height * 0.025))
+                            .multilineTextAlignment(.center).padding().foregroundColor(CustomColor.selectionblue))
+                    .frame(width: geometry.size.width * 0.25 ,height: geometry.size.height * 0.1, alignment: .bottom)
+                    .position(x: geometry.size.width * 0.34 ,y: geometry.size.height * 0.6)
+                
+                
+                NextStepButton().onTapGesture {
+                    currentScene += 1
+                }.disabled(!change)
+                
+                .opacity(change ? 1 : 0)
+                
+                
+            } // :zstack
+            
+            
         } // :geometry reader
         
     }
